3574f9e47c8077abd25d3cedaae1876460d87e6b
Wilkuu002 <Kacperwilk@poczta.onet.pl>
2025-02-25 15:16:30 +0100

    login tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -84,7 +84,7 @@ function RootLayoutNav() {
         }}
       >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+        <Stack.Screen name="login" options={{ presentation: "modal" }} />
       </Stack>
     </ThemeProvider>
   );
